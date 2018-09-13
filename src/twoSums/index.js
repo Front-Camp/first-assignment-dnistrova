4fdaa9969499848f9ed7892ed6677b63afc76873
Nataliya Mykytyuk <41234940+dnistrova@users.noreply.github.com>
2018-09-13 20:50:25 +0300

    Update index.js

--- src/twoSums/index.js
+++ src/twoSums/index.js
@@ -9,7 +9,15 @@
  * twoSums([5, 9, 3], 12); // [9, 3]
  */
 const twoSums = (arr, base) => {
-  /* your logic here...*/
+let results = [];
+  for (let i=0; i<arr.length; i++) {
+    for (let j=i+1; j<arr.length; j++) {
+      if (arr[j] === base - arr[i]) {
+        results.push([arr[i], arr[j]])
+      }
+    }
+  }
+  return results;
 };
 
 export default twoSums;
